e5cc10ad8c86818b980ae09d0a9636f5d70a2631
Shan Lim <shanlim8312@gmail.com>
2020-08-21 16:38:01 +0000

    I finished the assignment

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+Shan Lim
+AP Computer Science
+8/21/2020
+
+*/
+
+public class HelloWorld{
+  public static void main(String args[]){
+
+    System.out.print("        _   ,_,   _");
+    System.out.println();
+    System.out.print("       | `'=) (='` |");
+    System.out.println();
+    System.out.print("      |.-.-.| /.-.-.|");
+    System.out.println();
+    System.out.print("      m      ''      m");
+
+
+
+    //This prints out a bat
+
+
+  }
+
+}
